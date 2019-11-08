9ec48f853da967acaaac998a33409f575e906d4c
Antony Sanders <antony.devon92@gmail.com>
2019-11-08 19:24:41 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,11 @@
 #write your code here
 
 def countdown
-  counter = 10 
+  counter = 10
+  until counter = 0 
+  puts "#{counter} SECONDS!"
+  counter -= 1 
+  pause 
   
   puts "HAPPY NEW YEAR!"
 end
