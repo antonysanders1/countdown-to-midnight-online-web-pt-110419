eead3e57914637380af4582405a397a009209685
Antony Sanders <antony.devon92@gmail.com>
2019-11-08 19:27:56 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,7 +4,7 @@ def countdown
   counter = 10
   until counter = 0 
   puts "#{counter} SECOND(S)!"
- # sleep 1
+  sleep 1
   counter -= 1 
 end
   
@@ -13,6 +13,5 @@ end
 
 
 def countdown_with_sleep
-
-
+  sleep 5
 end
